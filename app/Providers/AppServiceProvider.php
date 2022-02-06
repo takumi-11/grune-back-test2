a169a9853230b62cc89dacaddae4b2154e31018d
takumi-11 <taku.11.winter.9@gmail.com>
2022-02-06 22:21:29 +0900

    commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+$table->string('email', 191)->unique();
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Set only for local development machine so that we can run the command via CLI
+        if ($this->app->environment() == 'local') {
+            $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
+            $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
+            $this->app->register(\Reliese\Coders\CodersServiceProvider::class);
+            $this->app->register(DuskServiceProvider::class);
+        }
+    }
+}
